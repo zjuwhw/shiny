8163ce35a0c7445bdae53cbd911ac4d5f3cfc7ee
zjuwhw <zju.whw@gmail.com>
2020-07-10 14:46:22 +0800

    snapshot

--- ldfrq/app.R
+++ ldfrq/app.R
@@ -1,27 +1,50 @@
 library(shiny)
 
 ui <- fluidPage(
+    withMathJax(),
     titlePanel("Allele Frequencies and the Linkage Disequilibrium"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput(inputId = "pA", label = "P(A)", value = 0.75, min = 0.5, max = 1),
-            sliderInput(inputId = "pB", label = "P(B)", value = 0.75, min = 0.5, max = 1),
-            sliderInput(inputId = "pAB", label = "P(AB)", value = 0, min = 0, max = 1)),
+            wellPanel(
+                h4("Allele Frequencies"),
+                sliderInput(inputId = "pA", label = "P(A)", value = 0.75, min = 0.5, max = 1),
+                sliderInput(inputId = "pB", label = "P(B)", value = 0.75, min = 0.5, max = 1)
+            ),
+            wellPanel(
+                h4("Notation for Haplotype and Allele Frequencies"),
+                tableOutput("table"),
+                span("A and B are the major alleles for locus 1 and 2")
+            )
+        ),
         mainPanel(
-            plotOutput("plot"),
-            textOutput("haplotype"),
-            tableOutput("table1"),
-            tableOutput("table2"),
-            textOutput("ld"))
+            wellPanel(
+                h5("The range of haplotype frequencies and LD measures:"),
+                uiOutput("rangepAB"),
+                uiOutput("rangeD"),
+                uiOutput("rangeDprime"),
+                uiOutput("ranger2")
+            ),
+            plotOutput("plot")
+        )
     )
 )
 
+
 server <- function(input, output){
+    output$table = renderTable({
+        cbind("Locus" = c("A", "a", "Frequency"),
+              B = c("P(AB)", "P(aB)", "P(B)"),
+              b = c("P(Ab)", "P(ab)", "P(b)"),
+              Frequency = c("P(A)", "P(a)", "1"))
+    })
+
     minnum = reactive({input$pA+input$pB-1 })
     maxnum = reactive({min(input$pA,input$pB)})
-    output$haplotype = renderText({
-        paste("p(AB): ", minnum()," - ", maxnum(), sep="")
-    })
+    output$rangepAB = renderText({paste("p(AB): from ", minnum()," to ", maxnum(), sep="")})
+    output$rangeD = renderText({paste("D: from ",minnum()-input$pA*input$pB, " to ", maxnum()-input$pA*input$pB, sep="")})
+    output$rangeDprime = renderText({"D': from -1 to 1"})
+    output$ranger2 = renderText({paste("r2: from 0 to ", min(input$pA*(1-input$pB)/input$pB/(1-input$pA),input$pB*(1-input$pA)/input$pA/(1-input$pB)),sep="")})
+    
     output$plot = renderPlot({
         pABseq = seq(from=minnum(), to=maxnum(), length.out=100)
         D = pABseq-input$pA*input$pB
@@ -29,29 +52,11 @@ server <- function(input, output){
         r2 = D^2/(input$pA*input$pB*(1-input$pA)*(1-input$pB))
         layout(matrix(1:3,nrow=1))
         plot(pABseq, D, type="l", xlab="p(AB)", ylab="D")
+        abline(v=input$pA*input$pB,col="red")
         plot(pABseq, Dprime, type="l", xlab="p(AB)",ylab="D'")
+        abline(v=input$pA*input$pB,col="red")
         plot(pABseq, r2, type="l", xlab="p(AB)", ylab="r2")
-    })
-    output$table1 = renderTable({
-        cbind("Locus" = c("A", "a", "Frequency"),
-              B = c("P(AB)", "P(aB)", "P(B)"),
-              b = c("P(Ab)", "P(ab)", "P(b)"),
-              Frequency = c("P(A)", "P(a)", "1"))
-    })
-    output$table2 = renderTable({
-        cbind(Locus = c("A", "a", "Frequency"), 
-              B = c(input$pAB, input$pB-input$pAB, input$pB),
-              b = c(input$pA-input$pAB, 1-input$pA-input$pB+input$pAB, 1-input$pB),
-              Frequency = c(input$pA,1-input$pA,1))
-    })
-    output$ld = renderText({
-        pA=input$pA
-        pB=input$pB
-        pAB=input$pAB
-        D = pAB-pA*pB
-        Dprime = ifelse(D>0, D/min(pA*(1-pB),pB*(1-pA)), D/min(pA*pB, (1-pA)*(1-pB)))
-        r2 = D^2/(pA*pB*(1-pA)*(1-pB))
-        paste("D = ",D, "; ", "D' = ", Dprime, "; ", "r2 = ", r2, ".", sep="")
+        abline(v=input$pA*input$pB,col="red")
     })
 }
 
